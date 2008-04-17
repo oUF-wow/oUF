84fbfed2f3d8e0736c06bb35bee53e7c4539d43d
Trond A Ekseth <troeks@gmail.com>
2008-04-17 15:51:42 +0200

    PvP updates the textures as it should.

--- elements/pvp.lua
+++ elements/pvp.lua
@@ -32,7 +32,12 @@
 function oUF:UNIT_FACTION(event, unit)
 	if(unit ~= self.unit) then return end
 
-	if(UnitIsPVP(self.unit)) then
+	local factionGroup = UnitFactionGroup(unit)
+	if(UnitIsPVPFreeForAll(unit)) then
+		self.PvP:SetTexture[[Interface\TargetingFrame\UI-PVP-FFA]]
+		self.PvP:Show()
+	elseif(factionGroup and UnitIsPVP(unit)) then
+		self.PvP:SetTexture([[Interface\TargetingFrame\UI-PVP-]]..factionGroup)
 		self.PvP:Show()
 	else
 		self.PvP:Hide()
