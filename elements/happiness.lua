10e837006fddb1e32758ec8ffe23ad4f749981d4
Trond A Ekseth <troeks@gmail.com>
2008-08-20 16:05:40 +0200

    Check if .Happiness actually exists.

--- elements/happiness.lua
+++ elements/happiness.lua
@@ -4,23 +4,25 @@ function oUF:UNIT_HAPPINESS(event, unit)
 	if(self:IsEventRegistered'UNIT_MAXHEALTH') then self:UNIT_MAXHEALTH(event, unit) end
 	if(self:IsEventRegistered'UNIT_MAXMANA') then self:UNIT_MAXMANA(event, unit) end
 
-	local happiness = GetPetHappiness()
-	local hunterPet = select(2, HasPetUI())
+	if(self.Happiness) then
+		local happiness = GetPetHappiness()
+		local hunterPet = select(2, HasPetUI())
 
-	if(not (happiness or hunterPet)) then
-		return self.Happiness:Hide()
+		if(not (happiness or hunterPet)) then
+			return self.Happiness:Hide()
+		end
+
+		self.Happiness:Show()
+		if(happiness == 1) then
+			self.Happiness:SetTexCoord(0.375, 0.5625, 0, 0.359375)
+		elseif(happiness == 2) then
+			self.Happiness:SetTexCoord(0.1875, 0.375, 0, 0.359375)
+		elseif(happiness == 3) then
+			self.Happiness:SetTexCoord(0, 0.1875, 0, 0.359375)
+		end
+
+		if(self.PostUpdateHappiness) then self:PostUpdateHappiness(event, unit, happiness) end
 	end
-
-	self.Happiness:Show()
-	if(happiness == 1) then
-		self.Happiness:SetTexCoord(0.375, 0.5625, 0, 0.359375)
-	elseif(happiness == 2) then
-		self.Happiness:SetTexCoord(0.1875, 0.375, 0, 0.359375)
-	elseif(happiness == 3) then
-		self.Happiness:SetTexCoord(0, 0.1875, 0, 0.359375)
-	end
-
-	if(self.PostUpdateHappiness) then self:PostUpdateHappiness(event, unit, happiness) end
 end
 
 table.insert(oUF.subTypes, function(self)
