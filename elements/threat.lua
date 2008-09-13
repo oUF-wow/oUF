e3b38e65cd9dbc8054f6b8e04d87af81959dda94
Tekkub <tekkub@gmail.com>
2008-09-13 14:58:57 -0600

    Flesh out threat module, totally drycoded

--- elements/threat.lua
+++ elements/threat.lua
@@ -1,1 +1,50 @@
+--[[
+	Elements handled: .Threat, .ThreatText
 
+	Functions that can be overridden from within a layout:
+	 - :PreUpdateThreat(event, unit)
+	 - :OverrideUpdateThreat(event, unit, unit2, isTanking, status, scaledPercent, rawPercent, threatValue)
+	 - :PostUpdateThreat(event, unit, unit2, isTanking, status, scaledPercent, rawPercent, threatValue)
+--]]
+
+function oUF:UNIT_THREAT_SITUATION_UPDATE(event, unit)
+	local threat, threattext = self.Threat, self.ThreatText
+	if not threat or not threattext then return end
+
+	if self.PreUpdateThreat then self:PreUpdateThreat(event, unit) end
+
+	if not unit or unit == self.feedbackUnit then
+		local unit2 = self.feedbackUnit ~= self.unit and self.unit or nil
+		local isTanking, status, scaledPercent, rawPercent, threatValue = UnitDetailedThreatSituation(self.feedbackUnit, unit2)
+
+		if self.OverrideUpdateThreat then self:OverrideUpdateThreat(event, unit, unit2, isTanking, status, scaledPercent, rawPercent, threatValue)
+		else
+			if status > 0 and IsThreatWarningEnabled() then
+				local r, g, b = GetThreatStatusColor(status)
+
+				if indicator then
+					indicator:SetVertexColor(r, g, b)
+					indicator:Show()
+				end
+
+				if threattext then
+					threattext:SetFormattedText("|cff%02x%02x%02x%d%%", r*255, g*255, b*255, percentage)
+					threattext:Show()
+				end
+			else
+				if indicator then indicator:Hide() end
+				if threattext then threattext:Hide() end
+			end
+		end
+
+		if self.PostUpdateThreat then self:PostUpdateThreat(event, unit, unit2, isTanking, status, scaledPercent, rawPercent, threatValue) end
+	end
+end
+
+table.insert(oUF.subTypes, function(self, unit)
+	if self.Threat or self.ThreatText then
+		self:RegisterEvent("UNIT_THREAT_SITUATION_UPDATE")
+		if unit:match("target") or unit == "focus" then self.feedbackUnit = "player" end
+	end
+end)
+oUF:RegisterSubTypeMapping("UNIT_THREAT_SITUATION_UPDATE")
