0c73d56f10ba3c462780e3ac96600bd632cbcc3c
Trond A Ekseth <troeks@gmail.com>
2008-09-13 12:26:16 +0200

    Add a fallback texture to leader if it is a texture.

--- elements/leader.lua
+++ elements/leader.lua
@@ -7,9 +7,14 @@ function oUF:PARTY_LEADER_CHANGED(event)
 end
 
 table.insert(oUF.subTypes, function(self)
-	if(self.Leader) then
+	local leader = self.Leader
+	if(leader) then
 		self:RegisterEvent"PARTY_LEADER_CHANGED"
 		self:RegisterEvent"PARTY_MEMBERS_CHANGED"
+
+		if(leader:IsObjectType"Texture" and not leader:GetTexture()) then
+			leader:SetTexture[[Interface\GroupFrame\UI-Group-LeaderIcon]]
+		end
 	end
 end)
 oUF:RegisterSubTypeMapping"PARTY_LEADER_CHANGED"
