16de5a49040cf415447057837036eafc3947ce02
Trond A Ekseth <troeks@gmail.com>
2008-10-06 16:59:14 +0200

    Removet the variable requirement for model based portraits.

--- elements/portraits.lua
+++ elements/portraits.lua
@@ -10,7 +10,7 @@ function oUF:UNIT_PORTRAIT_UPDATE(event, unit)
 	if(self.unit ~= unit) then return end
 
 	local portrait = self.Portrait
-	if(portrait.type == "3D") then
+	if(portrait:IsObjectType'Model') then
 		if(not UnitExists(unit) or not UnitIsConnected(unit) or not UnitIsVisible(unit)) then
 			portrait:SetModelScale(4.25)
 			portrait:SetPosition(0, 0, -1.5)
