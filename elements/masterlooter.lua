9163fb4d7317ee525e9ce5fedf5679ba12d95903
Trond A Ekseth <troeks@gmail.com>
2009-01-14 03:31:52 +0100

    Clean up the mess that was masterlooter.

--- elements/masterlooter.lua
+++ elements/masterlooter.lua
@@ -4,30 +4,25 @@ assert(global, 'X-oUF needs to be defined in the parent add-on.')
 local oUF = _G[global]
 
 local function Update(self, event)
-	local unit = ''
-	local method, id = GetLootMethod()
+	local unit
+	local method, pid, rid = GetLootMethod()
 	if(method == 'master') then
-		if(GetNumRaidMembers() > 1) then
-			for i = 1, MAX_RAID_MEMBERS do local _, _, _, _, _, _, _, _, _, _, loot = GetRaidRosterInfo(i)
-				if(loot) then
-					unit = 'raid'..i
-					break
-				end
-			end
-		else
-			if(id == 0) then
+		if(pid) then
+			if(pid == 0) then
 				unit = 'player'
 			else
-				unit = 'party'..id
+				unit = 'party'..pid
 			end
+		elseif(rid) then
+			unit = 'raid'..rid
 		end
 
-		if(self.unit == unit) then
+		if(unit == self.unit) then
 			self.MasterLooter:Show()
-		else
+		elseif(self.MasterLooter:IsShown()) then
 			self.MasterLooter:Hide()
 		end
-	else
+	elseif(self.MasterLooter:IsShown()) then
 		self.MasterLooter:Hide()
 	end
 end
@@ -39,7 +34,7 @@ local function Enable(self, unit)
 		self:RegisterEvent('RAID_ROSTER_UPDATE', Update)
 
 		if(masterlooter:IsObjectType('Texture') and not masterlooter:GetTexture()) then
-			masterlooter:SetTexture([=[Interface\GroupFrame\UI-Group-MasterLooter]=])
+			masterlooter:SetTexture([[Interface\GroupFrame\UI-Group-MasterLooter]])
 		end
 
 		return true
