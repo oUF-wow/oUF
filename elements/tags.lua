5b4c12603f67d0c72f4e8065da2ada0e3586c1a2
Trond A Ekseth <troeks@gmail.com>
2009-08-17 22:13:38 +0200

    Make the event registrator correctly work with tags that contain symbols.

--- elements/tags.lua
+++ elements/tags.lua
@@ -286,7 +286,7 @@ end
 local RegisterEvents = function(fontstr, tagstr)
 	-- Forcefully strip away any parentheses and the characters in them.
 	tagstr = tagstr:gsub('%b()', '')
-	for tag in tagstr:gmatch'[%[]%w+[%]]' do
+	for tag in tagstr:gmatch'[%[].-[%]]' do
 		local tagevents = tagEvents[tag]
 		if(tagevents) then
 			for event in tagevents:gmatch'%S+' do
