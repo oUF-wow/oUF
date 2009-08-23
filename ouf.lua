6e72dac88b334041fb4f4b3a6ce8b99fd117694d
Trond A Ekseth <troeks@gmail.com>
2009-08-23 18:18:38 +0200

    Allow headers to have unitsuffixes of multiple target's target.

--- ouf.lua
+++ ouf.lua
@@ -273,7 +273,7 @@ local initObject = function(unit, style, ...)
 			end
 		end
 
-		if(suffix == 'target' and (i == 1 and not showPlayer)) then
+		if(suffix and suffix:match'%w+target' and (i == 1 and not showPlayer)) then
 			enableTargetUpdate(object)
 		else
 			object:SetScript("OnEvent", OnEvent)
