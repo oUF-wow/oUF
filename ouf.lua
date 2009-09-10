87c1712dc83082789631bba67512f674950af16d
Zariel <c.bannister@gmail.com>
2009-09-10 18:21:30 +0100

    Fix OnShow not calling PEW correctly

--- ouf.lua
+++ ouf.lua
@@ -292,7 +292,9 @@ local initObject = function(unit, style, ...)
 
 		object:SetAttribute("*type1", "target")
 		object:SetScript("OnAttributeChanged", OnAttributeChanged)
-		object:SetScript("OnShow", object.PLAYER_ENTERING_WORLD)
+		object:SetScript("OnShow", function(self)
+			self:PLAYER_ENTERING_WORLD"PLAYER_ENTERING_WORLD"
+		end)
 
 		object:RegisterEvent"PLAYER_ENTERING_WORLD"
 
