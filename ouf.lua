32c35288bddef3fc4b8bc2628a504f378bd93ce6
Trond A Ekseth <troeks@gmail.com>
2010-02-02 14:44:45 +0100

    Merge :SetManyAttributes into :SpawnHeader so we actually get to set attributes before the state drive shows the header.

--- ouf.lua
+++ ouf.lua
@@ -532,7 +532,7 @@ function oUF:SetActiveStyle(name)
 	style = name
 end
 
-function oUF:SpawnHeader(overrideName, template, showSolo, showParty, showRaid)
+function oUF:SpawnHeader(overrideName, template, showSolo, showParty, showRaid, ...)
 	if(not style) then return error("Unable to create frame. No styles have been registered.") end
 
 	template = (template or 'SecureGroupHeaderTemplate')
@@ -541,9 +541,9 @@ function oUF:SpawnHeader(overrideName, template, showSolo, showParty, showRaid)
 	local header = CreateFrame('Frame', name, UIParent, template)
 	header.initialConfigFunction = walkObject
 	header.style = style
-	header.SetManyAttributes = SetManyAttributes
-
 	header:SetAttribute("template", "SecureUnitButtonTemplate")
+
+	SetManyAttributes(header, ...)
 
 	if(showSolo) then
 		header:SetAttribute('visibleSolo', true)
