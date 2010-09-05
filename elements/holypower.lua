53941b8cc964a121c0805a99908a1cde757bfa26
Evilpaul <yj589794@googlemail.com>
2010-09-06 00:10:04 +0100

    addition of HolyPower element

---
+++ elements/holypower.lua
@@ -0,0 +1,62 @@
+local CC = select(4, GetBuildInfo()) == 4e4
+if not CC then return end
+
+if select(2, UnitClass('player')) ~= 'PALADIN' then return end
+
+local parent, ns = ...
+local oUF = ns.oUF
+
+local HOLY_POWER_INDEX = HOLY_POWER_INDEX
+local MAX_HOLY_POWER = MAX_HOLY_POWER
+
+local Update = function(self, event, unit)
+	if self.unit ~= unit then return end
+
+	local hp = self.HolyPower
+
+	if hp.PreUpdate then hp:PreUpdate(unit) end
+
+	local num = UnitPower(unit, HOLY_POWER_INDEX)
+	for i = 1, MAX_HOLY_POWER do
+		if i <= num then
+			hp[i]:SetAlpha(1)
+		else
+			hp[i]:SetAlpha(0)
+		end
+	end
+
+	if hp.PostUpdate then hp:PostUpdate(unit) end
+end
+
+
+local Path = function(self, ...)
+	return (self.HolyPower.Override or Update) (self, ...)
+end
+
+
+local ForceUpdate = function(element)
+	return Path(element.__owner, 'ForceUpdate')
+end
+
+
+local function Enable(self)
+	local hp = self.HolyPower
+	if(hp) then
+		hp.__owner = self
+		hp.ForceUpdate = ForceUpdate
+
+		self:RegisterEvent('UNIT_POWER', Path)
+
+		return true
+	end
+end
+
+
+local function Disable(self)
+	local hp = self.HolyPower
+	if(hp) then
+		self:UnregisterEvent('UNIT_POWER', Path)
+	end
+end
+
+oUF:AddElement('SoulShards', Path, Enable, Disable)
