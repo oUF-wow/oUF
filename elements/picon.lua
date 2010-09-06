18dda87add9ed4204ec40ce058db8e030999d6d8
Evilpaul <yj589794@googlemail.com>
2010-09-06 23:18:30 +0100

    addition of PhaseIcon element

---
+++ elements/picon.lua
@@ -0,0 +1,51 @@
+local CC = select(4, GetBuildInfo()) == 4e4
+if(not CC) then return end
+
+local parent, ns = ...
+local oUF = ns.oUF
+
+local UnitInPhase = UnitInPhase
+
+local Update = function(self, event)
+	local inPhase = UnitInPhase(self.unit)
+	local picon = self.PhaseIcon
+
+	if(inPhase) then
+		picon:Hide()
+	else
+		picon:Show()
+	end
+end
+
+local Path = function(self, ...)
+	return (self.PhaseIcon.Override or Update) (self, ...)
+end
+
+local ForceUpdate = function(element)
+	return Path(element.__owner, 'ForceUpdate')
+end
+
+local Enable = function(self)
+	local picon = self.PhaseIcon
+	if(picon) then
+		picon.__owner = self
+		picon.ForceUpdate = ForceUpdate
+
+		self:RegisterEvent('UNIT_PHASE', Path)
+
+		if(picon:IsObjectType'Texture' and not picon:GetTexture()) then
+			picon:SetTexture[[Interface\TargetingFrame\UI-PhasingIcon]]
+		end
+
+		return true
+	end
+end
+
+local Disable = function(self)
+	local picon = self.PhaseIcon
+	if(picon) then
+		self:UnregisterEvent('UNIT_PHASE', Path)
+	end
+end
+
+oUF:AddElement('PhaseIcon', Path, Enable, Disable)
