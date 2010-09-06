095058d67b1b1d59480907bed1f8bbea32391eb8
Trond A Ekseth <troeks@gmail.com>
2010-09-06 16:51:24 +0200

    Run a round of sed.

--- elements/healprediction.lua
+++ elements/healprediction.lua
@@ -2,13 +2,13 @@ local _, ns = ...
 local oUF = ns.oUF
 
 local function Update(self, event, unit)
-	if self.unit ~= unit then return end
+	if(self.unit ~= unit) then return end
 
 	local hp = self.HealPrediction
 
-	if not hp then return end
+	if(not hp) then return end
 
-	if hp.PreUpdate then hp:PreUpdate(unit) end
+	if(hp.PreUpdate) then hp:PreUpdate(unit) end
 
 	local myIncomingHeal = UnitGetIncomingHeals(unit, 'player') or 0
 	local allIncomingHeal = UnitGetIncomingHeals(unit) or 0
@@ -20,42 +20,39 @@ local function Update(self, event, unit)
 		allIncomingHeal = maxHealth * hp.maxOverflow - health
 	end
 
-	if allIncomingHeal < myIncomingHeal then
+	if(allIncomingHeal < myIncomingHeal) then
 		myIncomingHeal = allIncomingHeal
 		allIncomingHeal = 0
 	else
 		allIncomingHeal = allIncomingHeal - myIncomingHeal
 	end
 
-	if hp.myBar then
+	if(hp.myBar) then
 		hp.myBar:SetMinMaxValues(0, maxHealth)
 		hp.myBar:SetValue(myIncomingHeal)
 		hp.myBar:Show()
 	end
 
-	if hp.otherBar then
+	if(hp.otherBar) then
 		hp.otherBar:SetMinMaxValues(0, maxHealth)
 		hp.otherBar:SetValue(allIncomingHeal)
 		hp.otherBar:Show()
 	end
 
-	if hp.PostUpdate then hp:PostUpdate(unit) end
+	if(hp.PostUpdate) then hp:PostUpdate(unit) end
 end
-
 
 local function Path(self, ...)
 	return (self.HealPrediction.Override or Update) (self, ...)
 end
 
-
 local ForceUpdate = function(element)
 	return Path(element.__owner, 'ForceUpdate')
 end
 
-
 local function Enable(self)
 	local hp = self.HealPrediction
-	if hp then
+	if(hp) then
 		hp.__owner = self
 		hp.ForceUpdate = ForceUpdate
 
@@ -63,25 +60,24 @@ local function Enable(self)
 		self:RegisterEvent('UNIT_MAXHEALTH', Path)
 		self:RegisterEvent('UNIT_HEALTH', Path)
 
-		if not hp.maxOverflow then
+		if(not hp.maxOverflow) then
 			hp.maxOverflow = 1.05
 		end
 
-		if hp.myBar and not hp.myBar:GetStatusBarTexture() then
-			hp.myBar:SetStatusBarTexture([=[Interface\TargetingFrame\UI-StatusBar]=])
+		if(hp.myBar and not hp.myBar:GetStatusBarTexture()) then
+			hp.myBar:SetStatusBarTexture([[Interface\TargetingFrame\UI-StatusBar]])
 		end
-		if hp.otherBar and not hp.otherBar:GetStatusBarTexture() then
-			hp.otherBar:SetStatusBarTexture([=[Interface\TargetingFrame\UI-StatusBar]=])
+		if(hp.otherBar and not hp.otherBar:GetStatusBarTexture()) then
+			hp.otherBar:SetStatusBarTexture([[Interface\TargetingFrame\UI-StatusBar]])
 		end
 
 		return true
 	end
 end
 
-
 local function Disable(self)
 	local hp = self.HealPrediction
-	if hp then
+	if(hp) then
 		self:UnregisterEvent('UNIT_HEAL_PREDICTION', Path)
 		self:UnregisterEvent('UNIT_MAXHEALTH', Path)
 		self:UnregisterEvent('UNIT_HEALTH', Path)
