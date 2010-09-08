0ebb65939b2b9dbc54e117d4e7d8a0c3e614eb96
Trond A Ekseth <troeks@gmail.com>
2010-09-08 18:09:43 +0200

    Allow powerType to be nil so holy power is correctly updated on login.

--- elements/holypower.lua
+++ elements/holypower.lua
@@ -10,7 +10,7 @@ local HOLY_POWER_INDEX = HOLY_POWER_INDEX
 local MAX_HOLY_POWER = MAX_HOLY_POWER
 
 local Update = function(self, event, unit, powerType)
-	if(self.unit ~= unit or powerType ~= 'HOLY_POWER') then return end
+	if(self.unit ~= unit or (powerType and powerType ~= 'HOLY_POWER')) then return end
 
 	local hp = self.HolyPower
 	if(hp.PreUpdate) then hp:PreUpdate(unit) end
