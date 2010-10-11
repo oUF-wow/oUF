78c6140e534677a5d2e96ca833be5baf65cfcd7e
Trond A Ekseth <troeks@gmail.com>
2010-10-11 20:43:44 +0200

    Add missing event and make some minor changes to the code.

--- elements/qicon.lua
+++ elements/qicon.lua
@@ -1,11 +1,11 @@
 local parent, ns = ...
 local oUF = ns.oUF
 
-local Update = function(self, event)
-	local isQuestBoss = UnitIsQuestBoss(self.unit)
-	local qicon = self.QuestIcon
+local Update = function(self, event, unit)
+	if(unit ~= self.unit) then return end
 
-	if(isQuestBoss) then
+	local qicon = self.QuestIcon
+	if(UnitIsQuestBoss(unit)) then
 		return qicon:Show()
 	else
 		return qicon:Hide()
@@ -17,7 +17,7 @@ local Path = function(self, ...)
 end
 
 local ForceUpdate = function(element)
-	return Path(element.__owner, 'ForceUpdate')
+	return Path(element.__owner, 'ForceUpdate', element.__owner.unit)
 end
 
 local Enable = function(self)
@@ -25,6 +25,8 @@ local Enable = function(self)
 	if(qicon) then
 		qicon.__owner = self
 		qicon.ForceUpdate = ForceUpdate
+
+		self:RegisterEvent('UNIT_CLASSIFICATION_CHANGED', Path)
 
 		if(qicon:IsObjectType'Texture' and not qicon:GetTexture()) then
 			qicon:SetTexture[[Interface\TargetingFrame\PortraitQuestBadge]]
@@ -35,6 +37,9 @@ local Enable = function(self)
 end
 
 local Disable = function(self)
+	if(self.QuestIcon) then
+		self:UnregisterEvent('UNIT_CLASSIFICATION_CHANGED', Path)
+	end
 end
 
 oUF:AddElement('QuestIcon', Path, Enable, Disable)
