230de7fef082b93a73b63c76800d4967a2a2a6d8
Trond A Ekseth <troeks@gmail.com>
2011-05-01 12:50:13 +0200

    UHF overlap with UH now, so we only have to register one of them \:D/.

--- elements/health.lua
+++ elements/health.lua
@@ -73,9 +73,10 @@ local Enable = function(self, unit)
 
 		if(health.frequentUpdates and not self:GetScript'OnUpdate') then
 			self:RegisterEvent('UNIT_HEALTH_FREQUENT', Path)
+		else
+			self:RegisterEvent('UNIT_HEALTH', Path)
 		end
 
-		self:RegisterEvent("UNIT_HEALTH", Path)
 		self:RegisterEvent("UNIT_MAXHEALTH", Path)
 		self:RegisterEvent('UNIT_CONNECTION', Path)
 
