84527536780ad1577d6825ebd96206c04232b432
Trond A Ekseth <troeks@gmail.com>
2011-07-03 01:08:04 +0200

    Apply a round of sed and some manual style changes.

--- elements/maintank.lua
+++ elements/maintank.lua
@@ -1,51 +1,50 @@
-
 local parent, ns = ...
-local oUF = ns.oUF or _G.oUF
+local oUF = ns.oUF or oUF
 
 local Update = function(self, event)
-    local raidID = UnitInRaid(self.unit)
-    local _, _, _, _, _, _, _, _, _, rinfo = GetRaidRosterInfo(raidID)
+	local raidID = UnitInRaid(self.unit)
+	local _, _, _, _, _, _, _, _, _, rinfo = GetRaidRosterInfo(raidID)
 
-    if (raidID and (rinfo == 'MAINTANK') and not UnitHasVehicleUI(self.unit)) then
-        self.MainTank:Show()
-    else
-        self.MainTank:Hide()
-    end
+	if(raidID and (rinfo == 'MAINTANK') and not UnitHasVehicleUI(self.unit)) then
+		self.MainTank:Show()
+	else
+		self.MainTank:Hide()
+	end
 end
 
 local Path = function(self, ...)
-    return (self.MainTank.Override or Update)(self, ...)
+	return (self.MainTank.Override or Update)(self, ...)
 end
 
 local ForceUpdate = function(element)
-    return Path(element.__owner, 'ForceUpdate')
+	return Path(element.__owner, 'ForceUpdate')
 end
 
 local Enable = function(self)
-    local mt = self.MainTank
+	local mt = self.MainTank
 
-    if (mt) then
-        mt.__owner = self
-        mt.ForceUpdate = ForceUpdate
+	if(mt) then
+		mt.__owner = self
+		mt.ForceUpdate = ForceUpdate
 
-        self:RegisterEvent('PARTY_MEMBERS_CHANGED', Path)
-        self:RegisterEvent('RAID_ROSTER_UPDATE', Path)
+		self:RegisterEvent('PARTY_MEMBERS_CHANGED', Path)
+		self:RegisterEvent('RAID_ROSTER_UPDATE', Path)
 
-        if (mt:IsObjectType('Texture') and not mt:GetTexture()) then
-            mt:SetTexture('Interface\\GROUPFRAME\\UI-GROUP-MAINTANKICON')
-        end
+		if(mt:IsObjectType'Texture' and not mt:GetTexture()) then
+			mt:SetTexture[[Interface\GROUPFRAME\UI-GROUP-MAINTANKICON]]
+		end
 
-        return true
-    end
+		return true
+	end
 end
 
 local Disable = function(self)
-    local mt = self.MainTank
+	local mt = self.MainTank
 
-    if (mt) then
-        self:UnregisterEvent('PARTY_MEMBERS_CHANGED', Path)
-        self:UnregisterEvent('RAID_ROSTER_UPDATE', Path)
-    end
+	if (mt) then
+		self:UnregisterEvent('PARTY_MEMBERS_CHANGED', Path)
+		self:UnregisterEvent('RAID_ROSTER_UPDATE', Path)
+	end
 end
 
 oUF:AddElement('MainTank', Path, Enable, Disable)
