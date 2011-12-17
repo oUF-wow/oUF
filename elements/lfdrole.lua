1d2812d61ecf98042e3e6171c830f99a0c5a5414
Trond A Ekseth <troeks@gmail.com>
2011-12-17 16:58:54 +0100

    lfdrole: Add {Pre,Post}Update.

--- elements/lfdrole.lua
+++ elements/lfdrole.lua
@@ -4,13 +4,20 @@ local oUF = ns.oUF
 local Update = function(self, event)
 	local lfdrole = self.LFDRole
 
-	local role = UnitGroupRolesAssigned(self.unit)
+	if(lfdrole.PreUpdate) then
+		lfdrole:PreUpdate()
+	end
 
+	local role = UnitGroupRolesAssigned(self.unit)
 	if(role == 'TANK' or role == 'HEALER' or role == 'DAMAGER') then
 		lfdrole:SetTexCoord(GetTexCoordsForRoleSmallCircle(role))
 		lfdrole:Show()
 	else
 		lfdrole:Hide()
+	end
+
+	if(lfdrole.PostUpdate) then
+		lfdrole:PostUpdate(role)
 	end
 end
 
