12fca32b915aa17379ec4340615ced6c4355f91b
Trond A Ekseth <troeks@gmail.com>
2011-12-17 17:22:52 +0100

    pvp: Don't check for the existence of self.PvP.

--- elements/pvp.lua
+++ elements/pvp.lua
@@ -4,17 +4,15 @@ local oUF = ns.oUF
 local Update = function(self, event, unit)
 	if(unit ~= self.unit) then return end
 
-	if(self.PvP) then
-		local factionGroup = UnitFactionGroup(unit)
-		if(UnitIsPVPFreeForAll(unit)) then
-			self.PvP:SetTexture[[Interface\TargetingFrame\UI-PVP-FFA]]
-			self.PvP:Show()
-		elseif(factionGroup and UnitIsPVP(unit)) then
-			self.PvP:SetTexture([[Interface\TargetingFrame\UI-PVP-]]..factionGroup)
-			self.PvP:Show()
-		else
-			self.PvP:Hide()
-		end
+	local factionGroup = UnitFactionGroup(unit)
+	if(UnitIsPVPFreeForAll(unit)) then
+		self.PvP:SetTexture[[Interface\TargetingFrame\UI-PVP-FFA]]
+		self.PvP:Show()
+	elseif(factionGroup and UnitIsPVP(unit)) then
+		self.PvP:SetTexture([[Interface\TargetingFrame\UI-PVP-]]..factionGroup)
+		self.PvP:Show()
+	else
+		self.PvP:Hide()
 	end
 end
 
