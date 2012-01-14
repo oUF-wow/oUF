ae3ead641a935c628227b6080111c3eef9e49207
Trond A Ekseth <troeks@gmail.com>
2012-01-14 23:58:24 +0100

    runebar: Check for a valid texture before we set colors.

--- elements/runebar.lua
+++ elements/runebar.lua
@@ -74,13 +74,13 @@ local Enable = function(self, unit)
 
 		for i=1, 6 do
 			local rune = runes[i]
-			-- From my minor testing this is a okey solution. A full login always remove
-			-- the death runes, or at least the clients knowledge about them.
-			UpdateType(self, nil, i, math.floor((runemap[i]+1)/2))
-
 			if(rune:IsObjectType'StatusBar' and not rune:GetStatusBarTexture()) then
 				rune:SetStatusBarTexture[[Interface\TargetingFrame\UI-StatusBar]]
 			end
+
+			-- From my minor testing this is a okey solution. A full login always remove
+			-- the death runes, or at least the clients knowledge about them.
+			UpdateType(self, nil, i, math.floor((runemap[i]+1)/2))
 		end
 
 		self:RegisterEvent("RUNE_POWER_UPDATE", UpdateRune, true)
