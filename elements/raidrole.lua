e3fea16f2baf9f383d30c4ae0ab35a13371d295a
Adrian Lund-Lange <adrianlund@gmail.com>
2012-01-20 22:53:58 +0100

    maintank: Rename to RaidRole

--- elements/raidrole.lua
+++ elements/raidrole.lua
@@ -5,25 +5,25 @@ local Update = function(self, event)
 	local raidID = UnitInRaid(self.unit)
 	if(not raidID) then return end
 
-	local maintank = self.MainTank
-	if(maintank.PreUpdate) then
-		maintank:PreUpdate()
+	local raidrole = self.RaidRole
+	if(raidrole.PreUpdate) then
+		raidrole:PreUpdate()
 	end
 
 	local _, _, _, _, _, _, _, _, _, rinfo = GetRaidRosterInfo(raidID)
 	if(rinfo == 'MAINTANK' and not UnitHasVehicleUI(self.unit)) then
-		self.MainTank:Show()
+		raidrole:Show()
 	else
-		self.MainTank:Hide()
+		raidrole:Hide()
 	end
 
-	if(maintank.PostUpdate) then
-		return maintank:PostUpdate(rinfo)
+	if(raidrole.PostUpdate) then
+		return raidrole:PostUpdate(rinfo)
 	end
 end
 
 local Path = function(self, ...)
-	return (self.MainTank.Override or Update)(self, ...)
+	return (self.RaidRole.Override or Update)(self, ...)
 end
 
 local ForceUpdate = function(element)
@@ -31,17 +31,17 @@ local ForceUpdate = function(element)
 end
 
 local Enable = function(self)
-	local mt = self.MainTank
+	local raidrole = self.RaidRole
 
-	if(mt) then
-		mt.__owner = self
-		mt.ForceUpdate = ForceUpdate
+	if(raidrole) then
+		raidrole.__owner = self
+		raidrole.ForceUpdate = ForceUpdate
 
 		self:RegisterEvent('PARTY_MEMBERS_CHANGED', Path, true)
 		self:RegisterEvent('RAID_ROSTER_UPDATE', Path, true)
 
-		if(mt:IsObjectType'Texture' and not mt:GetTexture()) then
-			mt:SetTexture[[Interface\GROUPFRAME\UI-GROUP-MAINTANKICON]]
+		if(raidrole:IsObjectType'Texture' and not raidrole:GetTexture()) then
+			raidrole:SetTexture[[Interface\GROUPFRAME\UI-GROUP-MAINTANKICON]]
 		end
 
 		return true
@@ -49,12 +49,12 @@ local Enable = function(self)
 end
 
 local Disable = function(self)
-	local mt = self.MainTank
+	local raidrole = self.RaidRole
 
-	if (mt) then
+	if(raidrole) then
 		self:UnregisterEvent('PARTY_MEMBERS_CHANGED', Path)
 		self:UnregisterEvent('RAID_ROSTER_UPDATE', Path)
 	end
 end
 
-oUF:AddElement('MainTank', Path, Enable, Disable)
+oUF:AddElement('RaidRole', Path, Enable, Disable)
