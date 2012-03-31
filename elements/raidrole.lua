461a515329de9ae5abcdc08ad7d75adb5572ccd9
Trond A Ekseth <troeks@gmail.com>
2012-03-31 15:53:01 +0200

    raidrole: Initial documentation.

--- elements/raidrole.lua
+++ elements/raidrole.lua
@@ -1,3 +1,34 @@
+--[[ Element: Raid Role Icon
+
+ Handles visibility and updating of `self.RaidRole` based upon the units
+ party assignment.
+
+ Widget
+
+ RaidRole - A Texture representing the units party assignment. This is can be
+            main tank, main assist or blank.
+
+ Notes
+
+ This element updates by changing the texture.
+
+ Examples
+
+   -- Position and size
+   local RaidRole = self:CreateTexture(nil, 'OVERLAY')
+   RaidRole:SetSize(16, 16)
+   RaidRole:SetPoint('TOPLEFT')
+   
+   -- Register it with oUF
+   self.RaidRole = RaidRole
+
+ Hooks
+
+ Override(self) - Used to completely override the internal update function.
+                  Removing the table key entry will make the element fall-back
+                  to its internal function again.
+]]
+
 local parent, ns = ...
 local oUF = ns.oUF
 
