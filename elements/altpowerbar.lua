b5e24cc917312c7bcf561cf23b2c28bf9b4ac000
Trond A Ekseth <troeks@gmail.com>
2012-11-25 14:32:46 +0100

    altpowerbar: Support tooltip.
    Fixes #163

--- elements/altpowerbar.lua
+++ elements/altpowerbar.lua
@@ -14,6 +14,11 @@
  .colorTexture  - Use the vertex color values returned by
                   UnitAlternatePowerTextureInfo to color the bar.
 
+ Notes
+
+ OnEnter and OnLeave handlers to display a tooltip will be set on the widget if
+ it is mouse enabled.
+
  Examples
 
    -- Position and size
@@ -33,6 +38,31 @@ local parent, ns = ...
 local oUF = ns.oUF
 
 local ALTERNATE_POWER_INDEX = ALTERNATE_POWER_INDEX
+
+--[[ :UpdateTooltip()
+
+ The function called when the widget is hovered. Used to populate the tooltip.
+
+ Arguments
+
+ self - The AltPowerBar element.
+]]
+local UpdateTooltip = function(self)
+	GameTooltip:SetText(self.powerName, 1, 1, 1)
+	GameTooltip:AddLine(self.powerTooltip, nil, nil, nil, 1)
+	GameTooltip:Show()
+end
+
+local OnEnter = function(self)
+	if(not self:IsVisible()) then return end
+
+	GameTooltip_SetDefaultAnchor(GameTooltip, self)
+	self:UpdateTooltip()
+end
+
+local OnLeave = function()
+	GameTooltip:Hide()
+end
 
 local UpdatePower = function(self, event, unit, powerType)
 	if(self.unit ~= unit or powerType ~= 'ALTERNATE') then return end
@@ -59,8 +89,10 @@ local UpdatePower = function(self, event, unit, powerType)
 	local cur = UnitPower(unit, ALTERNATE_POWER_INDEX)
 	local max = UnitPowerMax(unit, ALTERNATE_POWER_INDEX)
 
-	local barType, min = UnitAlternatePowerInfo(unit)
+	local barType, min, _, _, _, _, _, _, _, powerName, powerTooltip = UnitAlternatePowerInfo(unit)
 	altpowerbar.barType = barType
+	altpowerbar.powerName = powerName
+	altpowerbar.powerTooltip = powerTooltip
 	altpowerbar:SetMinMaxValues(min, max)
 	altpowerbar:SetValue(cur)
 
@@ -117,6 +149,15 @@ local Enable = function(self, unit)
 		self:RegisterEvent('UNIT_POWER_BAR_HIDE', Toggler)
 
 		altpowerbar:Hide()
+
+		if(altpowerbar:IsMouseEnabled()) then
+			altpowerbar:SetScript('OnEnter', OnEnter)
+			altpowerbar:SetScript('OnLeave', OnLeave)
+
+			if(not altpowerbar.UpdateTooltip) then
+				altpowerbar.UpdateTooltip = UpdateTooltip
+			end
+		end
 
 		if(unit == 'player') then
 			PlayerPowerBarAlt:UnregisterEvent'UNIT_POWER_BAR_SHOW'
