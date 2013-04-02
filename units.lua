45499957f0f5f22be6697b278a95c23d70e0b8a5
Erik Raetz <erik.raetz@googlemail.com>
2013-04-03 00:24:31 +0300

    units: Adding two generic boss events.

--- units.lua
+++ units.lua
@@ -14,6 +14,9 @@ function oUF:HandleUnit(object, unit)
 		object:RegisterEvent('UPDATE_MOUSEOVER_UNIT', object.UpdateAllElements)
 	elseif(unit == 'focus') then
 		object:RegisterEvent('PLAYER_FOCUS_CHANGED', object.UpdateAllElements)
+	elseif(unit:match'(boss)%d?$' == 'boss') then
+		object:RegisterEvent('INSTANCE_ENCOUNTER_ENGAGE_UNIT', object.UpdateAllElements, true)
+		object:RegisterEvent('UNIT_TARGETABLE_CHANGED', object.UpdateAllElements)
 	elseif(unit:match'%w+target') then
 		enableTargetUpdate(object)
 	end
