2c24ab83a3315fbcd86339839bb4f9866bc1d908
Trond A Ekseth <troeks@gmail.com>
2013-04-14 18:02:44 +0200

    core: Fall-back to pre-5.2 menus for raid frames.

--- ouf.lua
+++ ouf.lua
@@ -255,6 +255,9 @@ local initObject = function(unit, style, styleFunc, header, ...)
 			if(suffix == 'target') then
 				enableTargetUpdate(object)
 			end
+
+			-- Fall-back to the old menu system on header frames.
+			object.menu = Private.menu
 		end
 
 		Private.UpdateUnits(object, objectUnit)
@@ -473,7 +476,7 @@ do
 				end
 
 				frame:SetAttribute('*type1', 'target')
-				frame:SetAttribute('*type2', 'togglemenu')
+				frame:SetAttribute('*type2', 'menu')
 				frame:SetAttribute('toggleForVehicle', true)
 				frame:SetAttribute('oUF-guessUnit', unit)
 			end
