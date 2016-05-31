132a7bd30fd5ec0af8b06f6305a7a78595cffa30
Adrian L Lange <mail@p3lim.net>
2016-05-31 16:38:55 +0200

    stagger: The default MonkStaggerBar is parented to PlayerFrame, no need to hide it manually

--- elements/stagger.lua
+++ elements/stagger.lua
@@ -152,7 +152,6 @@ local Enable = function(self, unit)
 			element:SetStatusBarTexture[[Interface\TargetingFrame\UI-StatusBar]]
 		end
 
-		MonkStaggerBar.Show = MonkStaggerBar.Hide
 		MonkStaggerBar:UnregisterEvent'PLAYER_ENTERING_WORLD'
 		MonkStaggerBar:UnregisterEvent'PLAYER_SPECIALIZATION_CHANGED'
 		MonkStaggerBar:UnregisterEvent'UNIT_DISPLAYPOWER'
@@ -170,8 +169,6 @@ local Disable = function(self)
 		self:UnregisterEvent('UNIT_DISPLAYPOWER', VisibilityPath)
 		self:UnregisterEvent('PLAYER_TALENT_UPDATE', VisibilityPath)
 
-		MonkStaggerBar.Show = nil
-		MonkStaggerBar:Show()
 		MonkStaggerBar:UnregisterEvent'PLAYER_ENTERING_WORLD'
 		MonkStaggerBar:UnregisterEvent'PLAYER_SPECIALIZATION_CHANGED'
 		MonkStaggerBar:UnregisterEvent'UNIT_DISPLAYPOWER'
