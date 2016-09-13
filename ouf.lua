6ced698ea81635d3d6f597a9343478ed4b3dacec
Rainrider <rainrider.wow@gmail.com>
2016-09-13 17:48:02 +0200

    core:  update the pet frame properly after entering/exiting a vehicle
    Fixes https://github.com/oUF-wow/oUF/issues/257
    Closes https://github.com/oUF-wow/oUF/pull/258

--- ouf.lua
+++ ouf.lua
@@ -46,12 +46,7 @@ local updateActiveUnit = function(self, event, unit)
 		realUnit = 'target'
 	end
 
-	if(modUnit == "pet" and realUnit ~= "pet") then
-		modUnit = "vehicle"
-	end
-
-	-- Drop out if the event unit doesn't match any of the frame units.
-	if(not UnitExists(modUnit) or unit and unit ~= realUnit and unit ~= modUnit) then return end
+	if(not UnitExists(modUnit)) then return end
 
 	-- Change the active unit and run a full update.
 	if Private.UpdateUnits(self, modUnit, realUnit) then
@@ -213,8 +208,8 @@ local initObject = function(unit, style, styleFunc, header, ...)
 		end
 
 		if(not (suffix == 'target' or objectUnit and objectUnit:match'target')) then
-			object:RegisterEvent('UNIT_ENTERED_VEHICLE', updateActiveUnit, true)
-			object:RegisterEvent('UNIT_EXITED_VEHICLE', updateActiveUnit, true)
+			object:RegisterEvent('UNIT_ENTERED_VEHICLE', updateActiveUnit)
+			object:RegisterEvent('UNIT_EXITED_VEHICLE', updateActiveUnit)
 
 			-- We don't need to register UNIT_PET for the player unit. We register it
 			-- mainly because UNIT_EXITED_VEHICLE and UNIT_ENTERED_VEHICLE doesn't always
