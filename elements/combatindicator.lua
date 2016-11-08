c8b0adb0cd22b6ff7f34778947c1133bb6f1f031
Rainrider <rainrider.wow@gmail.com>
2016-11-08 06:01:40 +0100

    combatindicator: update the documentation

--- elements/combatindicator.lua
+++ elements/combatindicator.lua
@@ -1,38 +1,38 @@
---[[ Element: Combat Indicator
- Toggles the visibility of `self.CombatIndicator` based on the player's combat status.
+--[[
+# Element: Combat Indicator
 
- Widget
+Toggles the visibility of `self.CombatIndicator` based on the player's combat status.
 
- CombatIndicator - Any UI widget.
+## Widget
 
- Notes
+CombatIndicator - Any UI widget.
 
- The default assistant icon will be applied if the UI widget is a texture and
- doesn't have a texture or color defined.
+## Notes
 
- Examples
+The default assistant icon will be applied if the UI widget is a texture and doesn't have a texture or color defined.
 
-   -- Position and size
-   local CombatIndicator = self:CreateTexture(nil, 'OVERLAY')
-   CombatIndicator:SetSize(16, 16)
-   CombatIndicator:SetPoint('TOP', self)
+## Examples
 
-   -- Register it with oUF
-   self.CombatIndicator = CombatIndicator
+    -- Position and size
+    local CombatIndicator = self:CreateTexture(nil, 'OVERLAY')
+    CombatIndicator:SetSize(16, 16)
+    CombatIndicator:SetPoint('TOP', self)
 
- Hooks
-
- Override(self) - Used to completely override the internal update function.
-                  Removing the table key entry will make the element fall-back
-                  to its internal function again.
-
-]]
+    -- Register it with oUF
+    self.CombatIndicator = CombatIndicator
+--]]
 
 local parent, ns = ...
 local oUF = ns.oUF
 
 local function Update(self, event)
 	local element = self.CombatIndicator
+
+	--[[ Callback: CombatIndicator:PreUpdate()
+	Called before the element has been updated.
+
+	* self - the CombatIndicator element
+	--]]
 	if(element.PreUpdate) then
 		element:PreUpdate()
 	end
@@ -44,12 +44,24 @@ local function Update(self, event)
 		element:Hide()
 	end
 
+	--[[ Callback: CombatIndicator:PostUpdate(inCombat)
+	Called after the element has been updated.
+
+	* self     - the CombatIndicator element
+	* inCombat - a boolean indicating if the player is affectiong combat
+	--]]
 	if(element.PostUpdate) then
 		return element:PostUpdate(inCombat)
 	end
 end
 
 local function Path(self, ...)
+	--[[ Override: CombatIndicator:Override(...)
+	Used to completely override the internal update function.
+
+	* self - the CombatIndicator element
+	* ...  - the event and the arguments that accompany it
+	--]]
 	return (self.CombatIndicator.Override or Update) (self, ...)
 end
 
