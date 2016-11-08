6e98b75fcf0b79332017cea0bdddd463b8709de0
Rainrider <rainrider.wow@gmail.com>
2016-11-08 22:59:30 +0100

    questindicator: update the documentation

--- elements/questindicator.lua
+++ elements/questindicator.lua
@@ -1,33 +1,26 @@
---[[ Element: Quest Indicator
+--[[
+# Element: Quest Indicator
 
- Handles updating and toggles visibility based upon the units connection to a
- quest.
+Handles updating and toggles visibility based upon the units connection to a quest.
 
- Widget
+## Widget
 
- QuestIndicator - Any UI widget.
+QuestIndicator - Any UI widget.
 
- Notes
+## Notes
 
- The default quest icon will be used if the UI widget is a texture and doesn't
- have a texture or color defined.
+The default quest icon will be used if the UI widget is a texture and doesn't have a texture or color defined.
 
- Examples
+## Examples
 
-   -- Position and size
-   local QuestIndicator = self:CreateTexture(nil, 'OVERLAY')
-   QuestIndicator:SetSize(16, 16)
-   QuestIndicator:SetPoint('TOPRIGHT', self)
+    -- Position and size
+    local QuestIndicator = self:CreateTexture(nil, 'OVERLAY')
+    QuestIndicator:SetSize(16, 16)
+    QuestIndicator:SetPoint('TOPRIGHT', self)
 
-   -- Register it with oUF
-   self.QuestIndicator = QuestIndicator
-
- Hooks
-
- Override(self) - Used to completely override the internal update function.
-                  Removing the table key entry will make the element fall-back
-                  to its internal function again.
-]]
+    -- Register it with oUF
+    self.QuestIndicator = QuestIndicator
+--]]
 
 local parent, ns = ...
 local oUF = ns.oUF
@@ -36,6 +29,12 @@ local function Update(self, event, unit)
 	if(unit ~= self.unit) then return end
 
 	local element = self.QuestIndicator
+
+	--[[ Callback: QuestIndicator:PreUpdate()
+	Called before the element has been updated.
+
+	* self - the QuestIndicator element
+	--]]
 	if(element.PreUpdate) then
 		element:PreUpdate()
 	end
@@ -47,12 +46,24 @@ local function Update(self, event, unit)
 		element:Hide()
 	end
 
+	--[[ Callback: QuestIndicator:PostUpdate(isQuestBoss)
+	Called after the element has been updated.
+
+	* self        - the QuestIndicator element
+	* isQuestBoss - a Boolean indicating if the unit is a quest boss
+	--]]
 	if(element.PostUpdate) then
 		return element:PostUpdate(isQuestBoss)
 	end
 end
 
 local function Path(self, ...)
+	--[[ Override: QuestIndicator:Override(...)
+	Used to completely override the internal update function.
+
+	* self - the QuestIndicator element
+	* ...  - the event and the arguments that accompany it
+	--]]
 	return (self.QuestIndicator.Override or Update) (self, ...)
 end
 
