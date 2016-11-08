3a95964d6d752d920732dff6acb0645adfbc2f10
Rainrider <rainrider.wow@gmail.com>
2016-11-08 06:01:54 +0100

    grouproleindicator: update the documentation

--- elements/grouproleindicator.lua
+++ elements/grouproleindicator.lua
@@ -1,42 +1,40 @@
---[[ Element: Group Role Indicator
+--[[
+# Element: Group Role Indicator
 
- Toggles visibility of the assigned role icon based upon the units current dungeon
- role.
+Toggles visibility of the assigned role icon based upon the units current dungeon role.
 
- Widget
+## Widget
 
- GroupRoleIndicator - A Texture containing the LFD role icons at specific locations. Look
-           at the default LFD role icon texture for an example of this.
-           Alternatively you can look at the return values of
-           GetTexCoordsForRoleSmallCircle(role).
+GroupRoleIndicator - A Texture containing the LFD role icons at specific locations.
+                     See the default LFD role icon texture for an example of this.
+                     Alternatively look up the return values of `GetTexCoordsForRoleSmallCircle(role)`.
 
- Notes
+## Notes
 
- The default LFD role texture will be applied if the UI widget is a texture and
- doesn't have a texture or color defined.
+The default LFD role texture will be applied if the UI widget is a texture and doesn't have a texture or color defined.
 
- Examples
+## Examples
 
-   -- Position and size
-   local GroupRoleIndicator = self:CreateTexture(nil, 'OVERLAY')
-   GroupRoleIndicator:SetSize(16, 16)
-   GroupRoleIndicator:SetPoint('LEFT', self)
+    -- Position and size
+    local GroupRoleIndicator = self:CreateTexture(nil, 'OVERLAY')
+    GroupRoleIndicator:SetSize(16, 16)
+    GroupRoleIndicator:SetPoint('LEFT', self)
 
-   -- Register it with oUF
-   self.GroupRoleIndicator = GroupRoleIndicator
-
- Hooks
-
- Override(self) - Used to completely override the internal update function.
-                  Removing the table key entry will make the element fall-back
-                  to its internal function again.
-]]
+    -- Register it with oUF
+    self.GroupRoleIndicator = GroupRoleIndicator
+--]]
 
 local parent, ns = ...
 local oUF = ns.oUF
 
 local function Update(self, event)
 	local element = self.GroupRoleIndicator
+
+	--[[ Callback: GroupRoleIndicator:PreUpdate()
+	Called before the element has been updated.
+
+	* self - the GroupRoleIndicator element
+	--]]
 	if(element.PreUpdate) then
 		element:PreUpdate()
 	end
@@ -49,12 +47,24 @@ local function Update(self, event)
 		element:Hide()
 	end
 
+	--[[ Callback: GroupRoleIndicator:PostUpdate(role)
+	Called after the element has been updated.
+
+	* self - the GroupRoleIndicator element
+	* role - the role as returned by `UnitGroupRolesAssigned`
+	--]]
 	if(element.PostUpdate) then
 		return element:PostUpdate(role)
 	end
 end
 
 local function Path(self, ...)
+	--[[ Override: GroupRoleIndicator:Override(self, ...)
+	Used to completely override the internal update function.
+
+	* self - the GroupRoleIndicator element
+	* ...  - the event and the arguments that accompany it
+	--]]
 	return (self.GroupRoleIndicator.Override or Update) (self, ...)
 end
 
