d887eb01a7d670a0a241001f0f6ed6dcf5e5a4dc
Rainrider <rainrider.wow@gmail.com>
2017-03-11 03:03:13 +0100

    docs: events.lua

--- events.lua
+++ events.lua
@@ -52,6 +52,18 @@ local event_metatable = {
 	end,
 }
 
+--[[ frame:RegisterEvent(event, func, unitless)
+Used to register a frame for a game event and add an event handler.
+
+* self     - frame that will be registered for the given event. OnUpdate polled frames are prevented from registering
+             events
+* event    - name of the event to register
+* func     - function that will be executed when the event fires. If a string is passed, then a function by that name
+             must be defined on the frame. Multiple functions can be added for the same frame and event.
+* unitless - boolean indicating that the event does not fire for a specific unit, so the event arguments won't be
+             matched to the frame unit(s). Events that do not start with UNIT_ or are not known to be unit events are
+             automatically considered unitless
+--]]
 function frame_metatable.__index:RegisterEvent(event, func, unitless)
 	-- Block OnUpdate polled frames from registering events.
 	if(self.__eventless) then return end
@@ -101,6 +113,14 @@ function frame_metatable.__index:RegisterEvent(event, func, unitless)
 	end
 end
 
+--[[ frame:UnregisterEvent(event, func)
+Used to remove a function from the event handler list for a game event.
+
+* self  - frame registered for the event
+* event - name of the registered event
+* func  - function to be removed from the list of event handlers. If this is the only handler for the given event, then
+          the frame will be unregistered for the event
+--]]
 function frame_metatable.__index:UnregisterEvent(event, func)
 	argcheck(event, 2, 'string')
 
