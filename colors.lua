1dbe759eacc5df2f7c39a5d86c1c9708b5e363f1
Val Voronov <i.lightspark@gmail.com>
2017-04-25 17:23:02 +0700

    Move all colors to colors.lua.

--- colors.lua
+++ colors.lua
@@ -10,10 +10,12 @@ local colors = {
 		1, 1, 0,
 		0, 1, 0
 	},
+	health = {49 / 255, 207 / 255, 37 / 255},
 	disconnected = {.6, .6, .6},
-	tapped = {.6,.6,.6},
+	tapped = {.6, .6, .6},
 	class = {},
 	reaction = {},
+	power = {},
 }
 
 -- We do this because people edit the vars directly, and changing the default
@@ -55,6 +57,38 @@ end
 for eclass, color in next, FACTION_BAR_COLORS do
 	colors.reaction[eclass] = {color.r, color.g, color.b}
 end
+
+for power, color in next, PowerBarColor do
+	if (type(power) == 'string') then
+		if(type(select(2, next(color))) == 'table') then
+			colors.power[power] = {}
+
+			for index, color in next, color do
+				colors.power[power][index] = {color.r, color.g, color.b}
+			end
+		else
+			colors.power[power] = {color.r, color.g, color.b, atlas = color.atlas}
+		end
+	end
+end
+
+-- sourced from FrameXML/Constants.lua
+colors.power[0] = colors.power.MANA
+colors.power[1] = colors.power.RAGE
+colors.power[2] = colors.power.FOCUS
+colors.power[3] = colors.power.ENERGY
+colors.power[4] = colors.power.COMBO_POINTS
+colors.power[5] = colors.power.RUNES
+colors.power[6] = colors.power.RUNIC_POWER
+colors.power[7] = colors.power.SOUL_SHARDS
+colors.power[8] = colors.power.LUNAR_POWER
+colors.power[9] = colors.power.HOLY_POWER
+colors.power[11] = colors.power.MAELSTROM
+colors.power[12] = colors.power.CHI
+colors.power[13] = colors.power.INSANITY
+colors.power[16] = colors.power.ARCANE_CHARGES
+colors.power[17] = colors.power.FURY
+colors.power[18] = colors.power.PAIN
 
 local function colorsAndPercent(a, b, ...)
 	if(a <= 0 or b == 0) then
