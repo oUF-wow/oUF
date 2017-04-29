b17c671d0d98737301b4b856f9c7c141fe46a333
Adrian L Lange <mail@p3lim.net>
2017-04-29 15:36:47 +0200

    phaseindicator: Update Override documentation
    Because @Rainrider is picky

--- elements/phaseindicator.lua
+++ elements/phaseindicator.lua
@@ -56,7 +56,7 @@ local function Update(self, event)
 end
 
 local function Path(self, ...)
-	--[[ Override: PhaseIndicator:Override(event, ...)
+	--[[ Override: PhaseIndicator.Override(self, event, ...)
 	Used to completely override the internal update function.
 
 	* self  - the parent object
