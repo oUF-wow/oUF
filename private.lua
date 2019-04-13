6958bcb21cb8483cece89414f872a37db370f3b8
Val Voronov <i.lightspark@gmail.com>
2019-04-13 20:42:52 +0700

    Renamed Private.UnitExists to Private.unitExists
    Renamed its upvalues as well

--- private.lua
+++ private.lua
@@ -21,7 +21,7 @@ function Private.error(...)
 	Private.print('|cffff0000Error:|r ' .. string.format(...))
 end
 
-function Private.UnitExists(unit)
+function Private.unitExists(unit)
 	return unit and (UnitExists(unit) or ShowBossFrameWhenUninteractable(unit))
 end
 
