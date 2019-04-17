oUF-wow/oUF

commit ae7a12c61890a4ca04982fd0fe6ce768be120ce1

portrait: Fix updates for *target units (#399)

events.lua
@@ -66,8 +66,9 @@ registering events.
              automatically considered unitless (boolean)
 --]]
 function frame_metatable.__index:RegisterEvent(event, func, unitless)
-	-- Block OnUpdate polled frames from registering events.
-	if(self.__eventless) then return end
+	-- Block OnUpdate polled frames from registering events except for
+	-- UNIT_MODEL_CHANGED which is used for portrait updates.
+	if(self.__eventless and event ~= 'UNIT_MODEL_CHANGED') then return end
 
 	argcheck(event, 2, 'string')
 

elements/portrait.lua
@@ -36,7 +36,7 @@ local _, ns = ...
 local oUF = ns.oUF
 
 local function Update(self, event, unit)
-	if(self.unit ~= unit) then return end
+	if(not unit or not UnitIsUnit(self.unit, unit)) then return end
 
 	local element = self.Portrait
 
@@ -48,25 +48,29 @@ local function Update(self, event, unit)
 	--]]
 	if(element.PreUpdate) then element:PreUpdate(unit) end
 
-	if(element:IsObjectType('PlayerModel')) then
-		local guid = UnitGUID(unit)
-		if(not UnitExists(unit) or not UnitIsConnected(unit) or not UnitIsVisible(unit)) then
-			element:SetCamDistanceScale(0.25)
-			element:SetPortraitZoom(0)
-			element:SetPosition(0, 0, 0.5)
-			element:ClearModel()
-			element:SetModel([[Interface\Buttons\TalkToMeQuestionMark.m2]])
-			element.guid = nil
-		elseif(element.guid ~= guid or event == 'UNIT_MODEL_CHANGED') then
-			element:SetCamDistanceScale(1)
-			element:SetPortraitZoom(1)
-			element:SetPosition(0, 0, 0)
-			element:ClearModel()
-			element:SetUnit(unit)
-			element.guid = guid
+	local guid = UnitGUID(unit)
+	local isAvailable = UnitIsConnected(unit) and UnitIsVisible(unit)
+	if(event ~= 'OnUpdate' or element.guid ~= guid or element.state ~= isAvailable) then
+		if(element:IsObjectType('PlayerModel')) then
+			if(not isAvailable) then
+				element:SetCamDistanceScale(0.25)
+				element:SetPortraitZoom(0)
+				element:SetPosition(0, 0, 0.25)
+				element:ClearModel()
+				element:SetModel([[Interface\Buttons\TalkToMeQuestionMark.m2]])
+			else
+				element:SetCamDistanceScale(1)
+				element:SetPortraitZoom(1)
+				element:SetPosition(0, 0, 0)
+				element:ClearModel()
+				element:SetUnit(unit)
+			end
+		else
+			SetPortraitTexture(element, unit)
 		end
-	else
-		SetPortraitTexture(element, unit)
+
+		element.guid = guid
+		element.state = isAvailable
 	end
 
 	--[[ Callback: Portrait:PostUpdate(unit)
@@ -101,7 +105,6 @@ local function Enable(self, unit)
 		element.__owner = self
 		element.ForceUpdate = ForceUpdate
 
-		self:RegisterEvent('UNIT_PORTRAIT_UPDATE', Path)
 		self:RegisterEvent('UNIT_MODEL_CHANGED', Path)
 		self:RegisterEvent('UNIT_CONNECTION', Path)
 
@@ -126,7 +129,6 @@ local function Disable(self)
 	if(element) then
 		element:Hide()
 
-		self:UnregisterEvent('UNIT_PORTRAIT_UPDATE', Path)
 		self:UnregisterEvent('UNIT_MODEL_CHANGED', Path)
 		self:UnregisterEvent('PARTY_MEMBER_ENABLE', Path)
 		self:UnregisterEvent('UNIT_CONNECTION', Path)

commit b2ff93b2cd86bbbef6242361c5432f5c3df6314c

core: handle pass-through events for eventless frames as unitless
Fixes #497
This is a regression introduced after #399 by #472

events.lua
@@ -116,7 +116,7 @@ function frame_metatable.__index:RegisterEvent(event, func, unitless)
 			table.insert(curev, func)
 		end
 
-		if(unitless) then
+		if(unitless or self.__eventless) then
 			-- re-register the event in case we have mixed registration
 			registerEvent(self, event)
 			if(self.unitEvents) then
@@ -130,7 +130,7 @@ function frame_metatable.__index:RegisterEvent(event, func, unitless)
 			self:SetScript('OnEvent', onEvent)
 		end
 
-		if(unitless) then
+		if(unitless or self.__eventless) then
 			registerEvent(self, event)
 		else
 			self.unitEvents = self.unitEvents or {}