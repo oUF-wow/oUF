c41d4ab457ce25b7ceef17356ceeb27f055c3955
Wetxius <Wetxius@users.noreply.github.com>
2019-10-02 14:05:16 +0300

    Added our castBar icon if function return nil.

--- elements/castbar.lua
+++ elements/castbar.lua
@@ -130,7 +130,7 @@ local function UNIT_SPELLCAST_START(self, event, unit)
 	element:SetValue(0)
 
 	if(element.Text) then element.Text:SetText(text) end
-	if(element.Icon) then element.Icon:SetTexture(texture) end
+	if(element.Icon) then element.Icon:SetTexture(texture or "Interface\\Icons\\trade_engineering") end
 	if(element.Time) then element.Time:SetText() end
 
 	local shield = element.Shield
@@ -343,7 +343,7 @@ local function UNIT_SPELLCAST_CHANNEL_START(self, event, unit, _, spellID)
 	element:SetValue(duration)
 
 	if(element.Text) then element.Text:SetText(name) end
-	if(element.Icon) then element.Icon:SetTexture(texture) end
+	if(element.Icon) then element.Icon:SetTexture(texture or "Interface\\Icons\\trade_engineering") end
 	if(element.Time) then element.Time:SetText() end
 
 	local shield = element.Shield
