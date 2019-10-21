ed9c491dd72e636a33431ba20a22f968a5569932
Rainrider <Rainrider@users.noreply.github.com>
2019-10-21 20:28:18 +0200

    core: future proof classic check

--- init.lua
+++ init.lua
@@ -2,5 +2,5 @@ local parent, ns = ...
 ns.oUF = {}
 ns.oUF.Private = {}
 
-ns.oUF.isClassic = select(4, GetBuildInfo()) == 11302
+ns.oUF.isClassic = select(4, GetBuildInfo()) < 20000
 ns.oUF.isRetail = not ns.oUF.isClassic
