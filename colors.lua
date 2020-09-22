4cd7b74f43ed5922c81674ac19e4ac4130c65a63
Val Voronov <i.lightspark@gmail.com>
2020-09-22 21:37:53 +0700

    Add alternative power to oUF.colors

--- colors.lua
+++ colors.lua
@@ -117,6 +117,10 @@ colors.power[16] = colors.power.ARCANE_CHARGES
 colors.power[17] = colors.power.FURY
 colors.power[18] = colors.power.PAIN
 
+-- alternate power, sourced from FrameXML/CompactUnitFrame.lua
+colors.power.ALTERNATE = {0.7, 0.7, 0.6}
+colors.power[10] = colors.power.ALTERNATE
+
 for i = 0, 3 do
 	colors.threat[i] = {GetThreatStatusColor(i)}
 end
