3580fa88f726347607c5fd568d0556b3256b3447
pedrozc90 <pedrozc90@gmail.com>
2023-07-30 12:06:18 -0300

    adding project id variables

--- init.lua
+++ init.lua
@@ -1,3 +1,13 @@
 local _, ns = ...
 ns.oUF = {}
 ns.oUF.Private = {}
+
+-- toc file
+local Interface = select(4, GetBuildInfo())
+ns.oUF.Interface = Interface
+
+-- https://wowpedia.fandom.com/wiki/WOW_PROJECT_ID
+ns.oUF.isRetail = (WOW_PROJECT_ID == WOW_PROJECT_MAINLINE)
+ns.oUF.isClassic = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC)
+ns.oUF.isTBC = (WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC)
+ns.oUF.isWotLK = (WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC)
