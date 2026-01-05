0c4178e68d413dd6369e91947617ee143f324ccb
Adrian L Lange <git@p3lim.net>
2026-01-05 16:08:56 +0100

    enums: Add enum for selection type

--- enums.lua
+++ enums.lua
@@ -13,3 +13,26 @@ oUF.Enum.DispelType = {
 	Enrage = 9,
 	Bleed = 11,
 }
+
+oUF.Enum.SelectionType = {
+	-- https://warcraft.wiki.gg/wiki/API_UnitSelectionType
+	-- only keeping the useful ones (i.e. no spectator or housing stuff)
+	Hostile = 0,
+	Unfriendly = 1,
+	Neutral = 2,
+	Friendly = 3,
+	PlayerSimple = 4,
+	PlayerExtended = 5,
+	Party = 6,
+	PartyPvP = 7,
+	Friend = 8,
+	Dead = 9,
+	-- CommentatorBattleground1 = 10,
+	-- CommentatorBattleground2 = 11,
+	-- Self = 12,
+	PartyPvPInBattleground = 13,
+	-- CommentatorArena1 = 14,
+	-- CommentatorArena2 = 15,
+	RecentAlly = 16,
+	-- rest of the types are for housing, irrelevant for us
+}
