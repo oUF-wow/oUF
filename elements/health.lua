e9259b9b35ee1bdbb320426a1fbcb28a5111f843
Val Voronov <i.lightspark@gmail.com>
2026-01-10 23:08:29 +0700

    health: Fix docs

--- elements/health.lua
+++ elements/health.lua
@@ -39,8 +39,8 @@ The following options are listed by priority. The first check that returns true 
 .colorReaction     - Use `self.colors.reaction[reaction]` to color the bar based on the player's reaction towards the
                      unit. `reaction` is defined by the return value of
                      [UnitReaction](https://warcraft.wiki.gg/wiki/API_UnitReaction) (boolean)
-.colorSmooth       - Use `self.colors.health.curve` to color the bar with a smooth gradient based on the unit's current
-                     health percentage (boolean)
+.colorSmooth       - Use color curve from `self.colors.health` to color the bar with a smooth gradient based on the
+                     unit's current health percentage (boolean)
 .colorHealth       - Use `self.colors.health` to color the bar. This flag is used to reset the bar color back to default
                      if none of the above conditions are met (boolean)
 
