246e6b058341b1609be353a07d4c07c856d9ac95
Adrian L Lange <git@p3lim.net>
2026-02-14 17:05:14 +0100

    too big

--- elements/auras.lua
+++ elements/auras.lua
@@ -241,7 +241,7 @@ local function updateAura(element, unit, data, position)
 	if(button.DispelIndicator) then
 		if(element.showDispelIndicator) then
 			local spriteIndex = C_UnitAuras.GetAuraDispelTypeColor(unit, data.auraInstanceID, element.dispelIndicatorCurve)
-			button.DispelIndicator:SetSize(width / 4, width / 4)
+			button.DispelIndicator:SetSize(width / 2, width / 2)
 			-- UICombatTimelineWarningIcons is a 3x4 grid, cell size is 64, plus 2 pixels for offsets
 			button.DispelIndicator:SetSpriteSheetCell(spriteIndex, 3, 4, 66, 66)
 			button.DispelIndicator:Show()
