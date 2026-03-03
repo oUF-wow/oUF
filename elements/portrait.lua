b11ece69ce44f98243d7dc7e81c3a7a625cb2e56
Adrian L Lange <git@p3lim.net>
2026-03-03 18:51:22 +0100

    Split this shit :angry:

--- elements/portrait.lua
+++ elements/portrait.lua
@@ -54,7 +54,11 @@ local function Update(self, event, unit)
 
 	local guid = UnitGUID(unit)
 	local isAvailable = UnitIsConnected(unit) and UnitIsVisible(unit)
-	local hasStateChanged = event ~= 'OnUpdate' or (not issecretvalue(guid) and not issecretvalue(element.guid) and element.guid ~= guid) or element.state ~= isAvailable
+
+	local hasStateChanged = event ~= 'OnUpdate'
+		or (not issecretvalue(guid) and not issecretvalue(element.guid) and element.guid ~= guid)
+		or element.state ~= isAvailable
+
 	if(hasStateChanged) then
 		if(element:IsObjectType('PlayerModel')) then
 			if(not isAvailable) then
