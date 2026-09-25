9a65fc867756eb8920bd147eda9619e064357142
Adrian L Lange <git@p3lim.net>
2026-09-25 18:13:19 +0200

    Lint

--- elements/pvpindicator.lua
+++ elements/pvpindicator.lua
@@ -59,7 +59,7 @@ local function Update(self, event, unit)
 	end
 
 	local status, info
-	if GameVersion.PTR then
+	if(GameVersion.PTR) then
 		if(element.Badge and GameCompatibility.BattleForAzeroth and UnitIsHumanPlayer(unit)) then
 			info = UnitFrameUtil.GetUnitPvPIndicatorDisplayInfo(unit, true)
 
