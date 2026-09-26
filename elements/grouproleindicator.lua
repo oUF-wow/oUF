bd4b21c41de960fb668598fec238190c3aeaaeee
Adrian L Lange <p3lim@users.noreply.github.com>
2026-09-26 17:12:20 +0200

    grouproleindicator: Use new util (#914)
    Pretty much a copy of #913, it uses the same util just with different
    textures and options.

--- elements/grouproleindicator.lua
+++ elements/grouproleindicator.lua
@@ -13,7 +13,10 @@ A default texture will be applied if the widget is a Texture and doesn't have a 
 
 ## Options
 
-.useAtlasSize - Makes the element use preprogrammed atlas' size instead of its set dimensions (boolean)
+.useAtlasSize - (DEPRECATED) Makes the element use preprogrammed atlas' size instead of its set dimensions (boolean)
+.tankAtlas    - Overrides the default atlas texture for tank (string)
+.healerAtlas  - Overrides the default atlas texture for healer (string)
+.damageAtlas  - Overrides the default atlas texture for damage (string)
 
 ## Examples
 
@@ -28,6 +31,11 @@ A default texture will be applied if the widget is a Texture and doesn't have a 
 
 local _, ns = ...
 local oUF = ns.oUF
+local Private = oUF.Private
+
+local GameVersion = Private.GameVersion
+
+local STATE = {}
 
 local function Update(self, event)
 	local element = self.GroupRoleIndicator
@@ -41,29 +49,38 @@ local function Update(self, event)
 		element:PreUpdate()
 	end
 
-	local role = UnitGroupRolesAssignedEnum(self.__unit)
-	if(issecretvalue(role)) then
-		role = nil
-	end
+	local role -- TODO: remove in 12.1.5
 
-	if(role == Enum.LFGRole.Tank) then
-		element:SetAtlas('UI-LFG-RoleIcon-Tank-Micro-Raid', element.useAtlasSize)
-		element:Show()
-	elseif(role == Enum.LFGRole.Healer) then
-		element:SetAtlas('UI-LFG-RoleIcon-Healer-Micro-Raid', element.useAtlasSize)
-		element:Show()
-	elseif(role == Enum.LFGRole.Damage) then
-		element:SetAtlas('UI-LFG-RoleIcon-DPS-Micro-Raid', element.useAtlasSize)
-		element:Show()
+	if(GameVersion.PTR) then
+		-- we have to set the unit here, not during Enable, as the unit is not valid then
+		STATE[element].unit = self.__unit
+
+		UnitFrameUtil.UpdateUnitFrameRoleIcon(STATE[element])
 	else
-		element:Hide()
+		role = UnitGroupRolesAssignedEnum(self.__unit)
+		if(issecretvalue(role)) then
+			role = nil
+		end
+
+		if(role == Enum.LFGRole.Tank) then
+			element:SetAtlas('UI-LFG-RoleIcon-Tank-Micro-Raid', element.useAtlasSize)
+			element:Show()
+		elseif(role == Enum.LFGRole.Healer) then
+			element:SetAtlas('UI-LFG-RoleIcon-Healer-Micro-Raid', element.useAtlasSize)
+			element:Show()
+		elseif(role == Enum.LFGRole.Damage) then
+			element:SetAtlas('UI-LFG-RoleIcon-DPS-Micro-Raid', element.useAtlasSize)
+			element:Show()
+		else
+			element:Hide()
+		end
 	end
 
 	--[[ Callback: GroupRoleIndicator:PostUpdate(role)
 	Called after the element has been updated.
 
 	* self - the GroupRoleIndicator element
-	* role - the role as returned by [UnitGroupRolesAssignedEnum](https://warcraft.wiki.gg/wiki/API_UnitGroupRolesAssignedEnum) (number)
+	* role - (DEPRECATED) the role as returned by [UnitGroupRolesAssignedEnum](https://warcraft.wiki.gg/wiki/API_UnitGroupRolesAssignedEnum) (number)
 	--]]
 	if(element.PostUpdate) then
 		return element:PostUpdate(role)
@@ -91,10 +108,27 @@ local function Enable(self, unit)
 		element.__owner = self
 		element.ForceUpdate = ForceUpdate
 
-		if(unit == 'player') then
+		if(GameVersion.PTR) then
+			STATE[element] = {
+				roleIcon = element,
+				optionTable = {
+					displayRoleIcon = true,
+					textureMap = {
+						TANK = element.tankAtlas or 'UI-LFG-RoleIcon-Tank-Micro-Raid',
+						HEALER = element.healerAtlas or 'UI-LFG-RoleIcon-Healer-Micro-Raid',
+						DAMAGER = element.damageAtlas or 'UI-LFG-RoleIcon-DPS-Micro-Raid',
+						VEHICLE = '', -- otherwise it will show a vehicle icon (why Blizzard?)
+					}
+				}
+			}
+
 			self:RegisterEvent('PLAYER_ROLES_ASSIGNED', Path, true)
 		else
-			self:RegisterEvent('GROUP_ROSTER_UPDATE', Path, true)
+			if(unit == 'player') then
+				self:RegisterEvent('PLAYER_ROLES_ASSIGNED', Path, true)
+			else
+				self:RegisterEvent('GROUP_ROSTER_UPDATE', Path, true)
+			end
 		end
 
 		return true
@@ -106,8 +140,12 @@ local function Disable(self)
 	if(element) then
 		element:Hide()
 
-		self:UnregisterEvent('PLAYER_ROLES_ASSIGNED', Path)
-		self:UnregisterEvent('GROUP_ROSTER_UPDATE', Path, true)
+		if(GameVersion.PTR) then
+			self:UnregisterEvent('PLAYER_ROLES_ASSIGNED', Path)
+		else
+			self:UnregisterEvent('PLAYER_ROLES_ASSIGNED', Path)
+			self:UnregisterEvent('GROUP_ROSTER_UPDATE', Path, true)
+		end
 	end
 end
 
